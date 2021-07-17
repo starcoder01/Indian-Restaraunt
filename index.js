df4d986255c06855bccd1108695d4a25474aa2a8
Sneha Banga <79856550+starcoder01@users.noreply.github.com>
2021-07-18 00:01:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var a=document.querySelector('#bar');
+var b=document.querySelector('.bar-text');
+
+function myfun(){
+    if(b.style.display=="none")
+        b.style.display="block";
+    else
+        b.style.display="none";
+}
